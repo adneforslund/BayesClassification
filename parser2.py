c0f813d7914b6c8d47d04378bbb04361c1a5d551
Ådne Gutubakken Forslund <adne.forslund@student.uib.no>
2018-03-08 11:41:10 +0100

    litt mer shit

--- parser2.py
+++ parser2.py
@@ -1,19 +1,43 @@
+import os
+import glob
+import re
+
+
 from pathlib import Path
+
+negativeMean = 0
+positiveMean = 0
+
 path = Path('DATA/aclImdb/train/')
 dirs = [x for x in path.iterdir() if x.is_dir() and x.name == "neg" or x.name == "pos"]
 for d in dirs:
     print(d)
 
-import os
-import glob
-import re
 
-from collections import Counter
-import pandas as pd
 
 # Words blir en liste med tuples (word, 0) eller (word, 1), hvor 0 er negativ og 1 er positiv 
 words =[]
 
+def meanForWords(word, wordlist):
+    counter = 0
+    for (bestWord,c) in wordlist:
+        if bestWord == word:
+            counter += 1
+    return float(counter) / float(len(wordlist))
+
+    
+    
+
+def mean(clas, mean, wordlist):
+    counter = 0
+    for (w, c) in wordlist:
+        if c == clas:
+            counter += 1
+    mean = float(counter) / float(len(wordlist))
+    return mean
+
+def bayes(wordMean, classMean, pre):
+    return (wordMean*pre)/classMean
 
 #for å fjerne html/xml tags med regex, erstatter med mellomrom . Funker med føkka formatering også
 def remove_tags(text):
@@ -29,7 +53,7 @@ def remove_punctuation(text):
 for d in dirs:
     # åpner alle filer i en path, renser og kopierer til en annen path
     for file in glob.glob(str(d) + "/*.txt"):
-        print(file)
+       # print(file)
         infile = open(file)
         a = infile.readline()
         a_rm = remove_tags(a)
@@ -44,5 +68,6 @@ for d in dirs:
             
         infile.close()
 
-for word in words:
-    print(word)
+print(mean(0, negativeMean, words))
+print(mean(1, positiveMean, words))
+print(bayes(meanForWords("love", words),0.5, mean(1,1,words)))
